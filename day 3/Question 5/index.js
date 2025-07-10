34546678b0ef853163c77640fc8bf405705a4e9d
Adham ahmed gaber awad <aaggffetouh@gmail.com>
2025-07-10 09:11:22 +0300

    Delete day 3 directory
    Deleted to change name

--- day 3/Question 5/index.js
+++
@@ -1,8 +0,0 @@
-let field = document.getElementById("field");
-field.readOnly = true;
-let regex = /^[0-9]$/;
-document.addEventListener("keydown", function(event) {
-    if(regex.test(event.key)) {
-        field.value += event.key;
-    }
-});
